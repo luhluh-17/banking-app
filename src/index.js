3c417c52566db2989924f0c346dc99181294d7bc
Ellaine Dela Cruz <maellaineanis@gmail.com>
2022-06-26 00:42:22 +0800

    build login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/index.css'
+import Login from './pages/Login'
+import Main from './pages/Main'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+function App() {
+	return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/main' element={<Main />} />
+      </Routes>
+	  </BrowserRouter>
+  )
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/App.css'
+import Dashboard from '../components/Dashboard'
+
+function Main() {
+	return (
+		<div>
+			<Dashboard />
+		</div>
+	)
+}
+
+export default Main
