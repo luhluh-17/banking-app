c821620ed849aa6af23161d52b3b550b26c9a142
hugostanley <stanleyhugo06@gmail.com>
2022-06-28 21:39:38 +0800

    Fix merge conflict in index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import Login from './banking/pages/Login'
-import Main from './banking/pages/Main'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './banking/styles/index.css'
-
-import Budget from './budget/Budget'
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/main" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-=======
 import { BrowserRouter } from 'react-router-dom'
 import BangkingApp from './banking/BangkingApp'
 
 import './banking/styles/index.css'
 
->>>>>>> main
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
