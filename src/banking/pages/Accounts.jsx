7f428d36c1eddb2cf5581878ccd2f4859361a92f
luhluh-17 <castro.ralph17@gmail.com>
2022-06-28 13:53:00 +0800

    add initial pages

---
+++ src/banking/pages/Accounts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Accounts = () => {
+  return (
+    <section className='flex-col content'>
+      <h1>Accounts Page</h1>
+    </section>
+  )
+}
+
+export default Accounts
---
+++ src/banking/pages/Transactions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Transactions() {
+  return (
+    <section className='flex-col content'>
+      <h1>Transactions Page</h1>
+    </section>
+  )
+}
+
+export default Transactions
