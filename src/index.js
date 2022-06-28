853c68d7c2061b31583c44c1266c4b819a9f60f6
luhluh-17 <castro.ralph17@gmail.com>
2022-06-28 13:55:30 +0800

    refactor app routing

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Login from './banking/pages/Login'
-import Main from './banking/pages/Main'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import BangkingApp from './banking/BangkingApp'
 
 import './banking/styles/index.css'
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<Login />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/main' element={<Main />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <BangkingApp />
+    </BrowserRouter>
   </React.StrictMode>
 )
