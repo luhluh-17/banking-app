2ede62fe15f1cfda20932a598b1a4dc404c5ab92
hugostanley <stanleyhugo06@gmail.com>
2022-06-29 19:34:39 +0800

    Fix undo changes in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import BangkingApp from './banking/BangkingApp'
-import './banking/styles/index.css'
 
+import './css/main.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
