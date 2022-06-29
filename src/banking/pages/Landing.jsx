fcf3ea990d4f86ea2e09a80cf89e7fd5397136fa
Ellaine Dela Cruz <maellaineanis@gmail.com>
2022-06-29 21:17:21 +0800

    build landing page

--- src/banking/pages/Landing.jsx
+++ src/banking/pages/Landing.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import '../../css/landing.css'
+
 
 const Landing = () => {
+  const navigate = useNavigate()
+
+  const handleCustomerLoginBtn = () => {
+    {
+      navigate('/customer-login')
+    }
+  }
+
+  const handleEmployeeLoginBtn = () => {
+    {
+      navigate('/employee-login')
+    }
+  }
+
   return (
-    <main className='flex-col'>
-      <h1>Landing Page</h1>
-      <p>Hero Section Goes Here</p>
-      <Link to='/login' className='btn'>
-        Go to Login Page
-      </Link>
+    <React.Fragment>
+    <header>
+      <img src={require('../../assets/logo.png')} className='logo-s'/>
+      <button className='login-btn' onClick={handleCustomerLoginBtn}>Customer Login</button>
+    </header>
+    <main className='hero-section'>
+      <h1>Finally, a better banking experience</h1>
     </main>
+    <footer>
+      <p>DISCLAIMER: This site is for educational purposes only</p>
+      <button className='login-btn'onClick={handleEmployeeLoginBtn}>Employee Login</button>
+    </footer>
+    </React.Fragment>
   )
 }
 
