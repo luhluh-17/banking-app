1c4f85b17b3a4818e544d2bebfd6424c4c110ac8
hugostanley <stanleyhugo06@gmail.com>
2022-06-29 19:28:47 +0800

    Undo changes in index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import BangkingApp from './banking/BangkingApp'
-import BUdget from './budget/Budget'
 import './banking/styles/index.css'
-import Budget from './budget/Budget'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Budget />
+      <BangkingApp />
     </BrowserRouter>
   </React.StrictMode>
 )
