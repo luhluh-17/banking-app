728980569450bc89bc3988e0863e87ba9596161d
luhluh-17 <castro.ralph17@gmail.com>
2022-07-01 16:22:51 +0800

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import BangkingApp from './banking/BangkingApp'
-import Budget from './budget/Budget'
-import './css/main.css'
 import App from './App'
+import './css/main.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-     <App/>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
