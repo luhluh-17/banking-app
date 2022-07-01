2f3fc7f0864a45d4809504522b74f5ab212c1f4e
luhluh-17 <castro.ralph17@gmail.com>
2022-07-01 22:46:56 +0800

    withdraw/deposit functions move to update modal

--- src/banking/pages/AccountDetails.jsx
+++ src/banking/pages/AccountDetails.jsx
@@ -5,6 +5,7 @@ import User from '../../js/classes/user'
 import Transaction from '../../js/classes/transaction'
 import { getAllUsers } from '../../js/utils/localstorage'
 import Button from '../components/Button'
+import Modal from '../components/Modal'
 
 function AccountDetails() {
   const amountRef = useRef(null)
@@ -12,6 +13,10 @@ function AccountDetails() {
 
   const { userId } = useParams()
   const obj = getAllUsers().find(item => item.id === parseInt(userId))
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const toggleDialog = () => setIsDialogOpen(bool => !bool)
+  const closeDialog = () => setIsDialogOpen(false)
 
   const user = new User(
     obj.id,
@@ -27,80 +32,124 @@ function AccountDetails() {
   const [balance, setBalance] = useState(parseInt(user.balance))
   const [transactions, setTransactions] = useState(user.transactions)
 
-  const handleWithdraw = () => {
+  const handleUpdate = action => {
     if (amountRef.current.value === '') return
-    const amount = parseInt(amountRef.current.value)
 
-    if (amount > user.balance) {
-      alert('Insufficient Amount')
+    const amount = parseInt(amountRef.current.value)
+    if (action === 'Withdraw') {
+      const updatedBalance = balance - amount
+      if (updatedBalance < 0) {
+        alert('insufficient balance')
+        return
+      }
+      setBalance(updatedBalance)
     } else {
-      amountRef.current.value = ''
-      setBalance(balance - amount)
-
-      const id = new Date().getTime()
-      const transaction = new Transaction(id, 'Withdraw', 'posted', amount)
-
-      setTransactions(state => [...state, transaction])
+      setBalance(balance + amount)
     }
-  }
-
-  const handleDeposit = () => {
-    if (amountRef.current.value === '') return
-    const amount = parseInt(amountRef.current.value)
-    amountRef.current.value = ''
-    setBalance(balance + amount)
 
     const id = new Date().getTime()
-    const transaction = new Transaction(id, 'Deposit', 'posted', amount)
+    const transaction = new Transaction(id, action, 'Posted', amount)
 
     setTransactions(state => [...state, transaction])
+
+    amountRef.current.value = ''
+    closeDialog()
   }
 
   return (
-    <main className='flex-col'>
-      <div className='btn-container-header'>
-        <Button
-          icon={'arrow_back'}
-          text='Back'
-          className='btn-primary-border'
-          onClick={() => navigate(-1)}
-        />
-
-        <div className='flex-row'>
+    <>
+      <main className='flex-col'>
+        <div className='btn-container-header'>
           <Button
-            icon={'edit'}
-            text='Edit'
-            className='btn-secondary-border'
+            icon={'arrow_back'}
+            text='Back'
+            className='btn-primary-border'
             onClick={() => navigate(-1)}
           />
 
-          <Button
-            icon={'delete'}
-            text='Delete'
-            className='btn-danger-border'
-            onClick={() => navigate(-1)}
-          />
-        </div>
-      </div>
+          <div className='flex-row'>
+            <Button
+              icon={'edit'}
+              text='Edit'
+              className='btn-secondary-border'
+              onClick={() => navigate(-1)}
+            />
 
-      <section className='account-details'>
-        <div className='flex-row'>
-          <div>
-            <h5>{user.id}</h5>
-            <h2>{user.name}</h2>
-            <h4>{user.email}</h4>
+            <Button
+              icon={'delete'}
+              text='Delete'
+              className='btn-danger-border'
+              onClick={() => navigate(-1)}
+            />
           </div>
         </div>
-        <div>
-          <h2>{balance}</h2>
-          <h4>Current Balance</h4>
-        </div>
-      </section>
 
-      <section>
-        <TableTransaction title='Transactions' list={transactions} />
-      </section>
-    </main>
+        <section className='account-details'>
+          <div className='flex-row'>
+            <div>
+              <h5>{user.id}</h5>
+              <h2>{user.name}</h2>
+              <h4>{user.email}</h4>
+            </div>
+          </div>
+          <div>
+            <h2>{balance}</h2>
+            <h4>Current Balance</h4>
+          </div>
+        </section>
+
+        <section className='mt-1'>
+          <div className='btn-container-header'>
+            <h3>Transactions</h3>
+            <div className='flex-row'>
+              <Button
+                className={'btn-primary'}
+                text='Update Balance'
+                onClick={toggleDialog}
+              />
+              <Button
+                className={'btn-primary'}
+                text='Send Money'
+                onClick={toggleDialog}
+              />
+            </div>
+          </div>
+          <TableTransaction list={transactions} />
+        </section>
+      </main>
+
+      <Modal title='Update Balance' isOpen={isDialogOpen} onClose={closeDialog}>
+        <form>
+          <div>
+            <label className='form-label'>Amount</label>
+            <input
+              className='form-input'
+              type='number'
+              placeholder='Enter Amount'
+              step={'.01'}
+              ref={amountRef}
+            />
+          </div>
+          <div className='dialog-btn-container'>
+            <Button
+              text='Cancel'
+              className='btn-primary'
+              onClick={toggleDialog}
+            />
+            <Button
+              text='Deposit'
+              className='btn-primary'
+              onClick={() => handleUpdate('Deposit')}
+            />
+            <Button
+              text='Withdraw'
+              className='btn-primary'
+              onClick={() => handleUpdate('Withdraw')}
+            />
+          </div>
+        </form>
+      </Modal>
+    </>
   )
 }
 
