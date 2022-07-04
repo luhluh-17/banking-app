f534b052fc7a38750b829286f896afc5355223be
luhluh-17 <castro.ralph17@gmail.com>
2022-07-04 22:04:36 +0800

    add dummy users data

--- src/banking/data/users.js
+++ src/banking/data/users.js
@@ -1,8 +1,9 @@
 import User from '../../js/classes/user'
 
 const users = [
-  new User(1, 'John', 'Doe', 'johndoe@gmail.com', 'pass', 10000, [], []),
-  new User(2, 'Jane', 'Doe', 'janedoe@gmail.com', 'pass', 20000, [], []),
+  new User(1656942667530, 'Ralph', 'Castro', 10000, 'rc@gmail.com'),
+  new User(1656943293201, 'Stan', 'Ley', 10000, 'stan@gmail.com', 10000),
+  new User(1656943301797, 'El', 'laine', 10000, 'ellaine@gmail.com', 10000),
 ]
 
 export default users
