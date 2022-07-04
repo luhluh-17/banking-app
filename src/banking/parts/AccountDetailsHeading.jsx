997997432b8f4cebb87a994ad6147d52080059c9
luhluh-17 <castro.ralph17@gmail.com>
2022-07-05 02:12:27 +0800

    create a seperate component for account details

--- src/banking/parts/AccountDetailsHeading.jsx
+++ src/banking/parts/AccountDetailsHeading.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 function AccountDetailsHeading({ user }) {
   return (
-    <section className='account-details'>
-      <div className='flex-row'>
+    <section>
+      <div className='account-details'>
         <div>
-          <h5>{user.id}</h5>
           <h2>{user.name}</h2>
-          <h4>{user.email}</h4>
+          <p>{user.id}</p>
+          <p>{user.email}</p>
         </div>
-      </div>
-      <div>
-        <h2>{user.formattedBalance}</h2>
-        <h4>Current Balance</h4>
+
+        <div>
+          <h2 className='bal'>{user.formattedBalance}</h2>
+          <h4>Current Balance</h4>
+        </div>
       </div>
     </section>
   )
