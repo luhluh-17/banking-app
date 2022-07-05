1b61a5fec5ce405a70b79ceabd3f0cad121a0c3d
luhluh-17 <castro.ralph17@gmail.com>
2022-07-05 18:06:18 +0800

    add chart in overview

--- src/banking/pages/Overview.jsx
+++ src/banking/pages/Overview.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Bar } from 'react-chartjs-2'
+import { getAllTransactions } from '../../js/utils/localstorage'
 
 const Overview = () => {
+  const list = getAllTransactions()
+  const chartData = {
+    labels: list.map(item => item.description),
+    datasets: [
+      {
+        label: 'Transactions',
+        data: list.map(item => item.amount),
+
+        backgroundColor: ['hsla(224, 84%, 34%)', 'rgb(242, 169, 11)'],
+      },
+    ],
+  }
+
   return (
     <main>
-      <div className='sticky'>
       <h2 className='title'>Welcome Admin</h2>
-      </div>
+      <Bar data={chartData} />
     </main>
   )
 }
