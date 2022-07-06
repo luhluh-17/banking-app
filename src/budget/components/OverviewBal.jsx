fbd1a9524784aa03b1888a747ff7a427f947b2a0
hugostanley <stanleyhugo06@gmail.com>
2022-07-06 20:05:14 +0800

    Use function formatCurrency module

--- src/budget/components/OverviewBal.jsx
+++ src/budget/components/OverviewBal.jsx
@@ -1,19 +1,13 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../helper/Context'
+import FormatCurrency from '../helper/FormatCurrency'
 
 const OverviewBal = () => {
   const { user } = useContext(UserContext)
 
-  const formatCurrency = number => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'PHP',
-    }).format(number)
-  }
-
   return (
     <div className='balance'>
-      <h2>{formatCurrency(user.balance)}</h2>
+      <h2>{FormatCurrency(user.balance)}</h2>
       <h4>Current Balance</h4>
       
     </div>
